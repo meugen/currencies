ba2476f7a4de6af021a1baa5634acbce46df36b2
Yevgeniy Malyshev <meugen.82@gmail.com>
2020-02-09 15:54:00 +0200

    Refactored InjectWorkerFactory

--- app/src/main/java/meugeninua/android/currencies/app/di/InjectWorkerFactory.java
+++ app/src/main/java/meugeninua/android/currencies/app/di/InjectWorkerFactory.java
@@ -3,18 +3,24 @@ package meugeninua.android.currencies.app.di;
 import android.content.Context;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
-import androidx.core.util.ObjectsCompat;
 import androidx.work.ListenableWorker;
 import androidx.work.WorkerFactory;
 import androidx.work.WorkerParameters;
 import meugeninua.android.currencies.app.workers.SyncWorker;
 
+import java.util.HashMap;
+import java.util.Map;
+
 class InjectWorkerFactory extends WorkerFactory {
 
     private final AppComponent component;
+    private final Map<String, WorkerCreator<?>> creators;
 
     InjectWorkerFactory(AppComponent component) {
         this.component = component;
+
+        creators = new HashMap<>();
+        creators.put(SyncWorker.class.getName(), SyncWorker::new);
     }
 
     @Nullable
@@ -24,14 +30,18 @@ class InjectWorkerFactory extends WorkerFactory {
             @NonNull String workerClassName,
             @NonNull WorkerParameters workerParameters
     ) {
-        ListenableWorker worker = null;
-        if (ObjectsCompat.equals(workerClassName, SyncWorker.class.getName())) {
-            worker = new SyncWorker(appContext, workerParameters);
-        }
+        WorkerCreator<?> creator = creators.get(workerClassName);
+        ListenableWorker worker = creator == null ? null
+                : creator.create(appContext, workerParameters);
 
         if (worker instanceof Injector) {
             ((Injector) worker).inject(component);
         }
         return worker;
+    }
+
+    private interface WorkerCreator<W extends ListenableWorker> {
+
+        W create(Context context, WorkerParameters parameters);
     }
 }
